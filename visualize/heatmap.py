08f7841723c01008d4d326cee6cd0c70f2f639d8
Dengzhanhong <dengzhanhongdarren@outlook.com>
2021-02-08 16:38:43 +0800

    加入反群聚算法！修复bug,调整importance参数

--- visualize/heatmap.py
+++ visualize/heatmap.py
@@ -6,10 +6,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-fig = plt.figure(figsize=[200, 200], dpi=300)
-ax = fig.add_subplot(111)
-df = pd.read_csv('../alg/result/freq/num10times400.csv')
-print(df.values)
-
-sns.heatmap(df.values, annot=False, ax=ax, linewidths=0)
-plt.savefig('test2.jpg')
+if __name__ == '__main__':
+    fig = plt.figure(dpi=600)
+    ax = fig.add_subplot(111)
+    df = pd.read_csv('../alg/result/freq/num26time300.csv')
+    # print(df.values)
+    sns.heatmap(df.values, annot=False, ax=ax, linewidths=0)
+    plt.savefig('heat_num26time300.jpg')
